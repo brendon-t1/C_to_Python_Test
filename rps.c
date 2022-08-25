f21b06ce08aaf2ea14b8e57aa54c465af3aee8cf
Brendon <soupe.aux.tortues@gmail.com>
2022-08-24 23:18:09 -0700

    really basic C rps

---
+++ rps.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void rps(int choice){
+    // 1 is rock, 2 is paper, 3 is scissors
+
+    // random is computer's choice
+    int random = 1;
+
+    // generate random number from 1 - 3
+    srand(time(0));
+    random = rand() % 4;
+
+    // determine win, lose, draw and print result
+    if (random == 1){
+        if (choice == 2) {
+            printf("You Win");
+        }
+        else if (choice == 3) {
+            printf("You Lose");
+        }
+        else {
+            printf("Draw");
+        }
+    }
+    if (random == 2){
+        if (choice == 3) {
+            printf("You Win");
+        }
+        else if (choice == 1) {
+            printf("You Lose");
+        }
+        else {
+            printf("Draw");
+        }
+    }
+    if (random == 3){
+        if (choice == 1) {
+            printf("You Win");
+        }
+        else if (choice == 2) {
+            printf("You Lose");
+        }
+        else {
+            printf("Draw");
+        }
+    }
+
+}
+
+
+int main(){
+    // must pass in 1 - 3
+    rps(1);
+}
